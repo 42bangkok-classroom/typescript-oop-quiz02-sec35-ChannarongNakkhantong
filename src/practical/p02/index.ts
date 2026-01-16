9d516af945f8215008507c45ec35aa964cb56b6a
Channarong <channarongnakkhantong@gmail.com>
2026-01-16 10:42:12 +0700

    aaaaaa

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,12 @@
 export function getPostsByUser() {}
+let a = Number(process.argv[2])
+interface userId {
+  id: number;
+  title: string;
+}
+
+let u1: userId = {
+  id: a,
+  title: "sunt aut facere repellat provident occaecati"
+}
+console.log(u1);
