bf1a0f9cbff319e898aaa8a710e8dd60dc958a32
Channarong <channarongnakkhantong@gmail.com>
2026-01-16 10:33:39 +0700

    D kub

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,2 +1,11 @@
 export function safeFetchComment() {}
-
+let a = Number(process.argv[2])
+interface safeFetchComment {
+  id: number;
+  body: string;
+}
+let u1: safeFetchComment = {
+  id: a,
+  body: ""
+}
+console.log(u1);
